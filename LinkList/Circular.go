dfffed7f3d9bb13afdaeba4e5c19ce42b6104d20
FizzyElt <a0979150560@gmail.com>
2020-09-05 16:19:26 +0800

    circular insert method

--- LinkList/Circular.go
+++ LinkList/Circular.go
@@ -26,15 +26,44 @@ func (cll *CircularLinkList) Add(val int) *CircularNode {
 		return cll.Head
 	}
 	current := cll.Head
+
 	for current.Next != cll.Head {
 		current = current.Next
 	}
+
 	current.Next = &CircularNode{val, cll.Head}
+	cll.Size++
+
 	return cll.Head
 }
 
-func (cll *CircularLinkList) Insert() {
+func (cll *CircularLinkList) Insert(index int, val int) *CircularNode {
+	if cll.Head == nil {
+		cll.Head = &CircularNode{val, nil}
+		cll.Head.Next = cll.Head
+		cll.Size++
+		return cll.Head
+	}
 
+	current := cll.Head
+	count := 0
+
+	for current.Next != cll.Head {
+		if index == count {
+			node := &CircularNode{val, current.Next}
+			current.Next = node
+			cll.Size++
+			return cll.Head
+		}
+		current = current.Next
+		count++
+	}
+
+	node := &CircularNode{val, current.Next}
+	current.Next = node
+	cll.Size++
+	
+	return cll.Head
 }
 
 func (cll *CircularLinkList) Remove() {
