08b24f3a623187382123551bec18020e4b779b9f
FizzyElt <a0979150560@gmail.com>
2020-09-05 15:49:59 +0800

    circular Print method

--- LinkList/Circular.go
+++ LinkList/Circular.go
@@ -1,10 +1,43 @@
 package LinkList
 
+import "fmt"
+
+// CircularNode circular node
 type CircularNode struct {
 	Val  int
 	Next *CircularNode
 }
 
+//CircularLinkList circular linked list
 type CircularLinkList struct {
 	Head *CircularNode
+}
+
+func (cll *CircularLinkList) Add() {
+
+}
+
+func (cll *CircularLinkList) Insert() {
+
+}
+
+func (cll *CircularLinkList) Remove() {
+
+}
+
+func (cll *CircularLinkList) Reverse() {
+
+}
+
+func (cll *CircularLinkList) Print() {
+	if cll.Head == nil {
+		fmt.Println("no node")
+		return
+	}
+	current := cll.Head
+	for current.Next != cll.Head {
+		fmt.Printf("%d->", current.Val)
+	}
+
+	fmt.Println()
 }
